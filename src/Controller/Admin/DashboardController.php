9feafe6d7cbadff997510a3da427a31d745a4a08
damien <contact.damiendiaz@gmail.com>
2022-06-02 13:05:17 +0200

    password security and easy admin installation

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
+
+class DashboardController extends AbstractDashboardController
+{
+    public function __construct(
+        private AdminUrlGenerator $adminUrlGenerator
+    ) {
+    }
+
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        $url = $this->adminUrlGenerator->setController(UserCrudController::class)
+            ->generateUrl();
+
+        return $this->redirect($url);
+
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+         $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+         // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Ma boutique');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Utilisateur', 'fa fa-user', User::class);
+    }
+}
