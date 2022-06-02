4d1b1f77f821201663a2196ccc4a6a7b739e5ac5
damien <contact.damiendiaz@gmail.com>
2022-06-02 19:48:48 +0200

    easy admin and add first product and crud

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Category;
+use App\Entity\Product;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -45,5 +47,7 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Utilisateur', 'fa fa-user', User::class);
+        yield MenuItem::linkToCrud('Catégories', 'fa fa-list', Category::class);
+        yield MenuItem::linkToCrud('Produits', 'fa-brands fa-envira', Product::class);
     }
 }
